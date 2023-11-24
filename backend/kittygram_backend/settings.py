d725a2014693bd69f98e293b8ef0cb98a93ff23a
Павел Никуленок <pashaaaa86@gmail.com>
2023-11-24 16:32:42 +0500

    test3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['62.84.123.149', '0.0.0.0', '127.0.0.1', 'localhost',
-                 'infra-sprint1.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
+                          default='127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -60,7 +63,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432),
+        'DB_PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
